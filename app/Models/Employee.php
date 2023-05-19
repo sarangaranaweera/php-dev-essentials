202b27a094f48a8f87a7d13e511253d0f82ea86b
saranga ranaweera <saranga@360-css.com>
2023-05-19 17:16:46 +0530

    3

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -6,7 +6,7 @@ namespace App\Models;
 
 class Employee                                      
 {
-    protected $name;
+    protected $name;     
     protected $age;
 
     public function setName($name)
